7c62a09eea9acf3599815b49558279f2e190a273
Junior Jimenez <klys2007@gmail.com>
2023-11-18 17:26:07 -0500

    animations added

--- Assets/PlayerActionController.cs
+++ Assets/PlayerActionController.cs
@@ -6,6 +6,8 @@ public class PlayerActionController : MonoBehaviour
 {
 
     private Rigidbody2D playerPhysics;
+    private SpriteRenderer spriteRenderer;
+    private Animator playerAnimator;
 
     // Start is called before the first frame update
     [Header("Physic Variables")]
@@ -15,6 +17,8 @@ public class PlayerActionController : MonoBehaviour
     void Start()
     {
         playerPhysics = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -24,6 +28,18 @@ public class PlayerActionController : MonoBehaviour
         if (horizontalDirection != 0)
         {
             playerPhysics.velocity = new Vector3(horizontalDirection * movementForce, playerPhysics.velocity.y, 0);
+
+            playerAnimator.SetBool("Running", true);
+            if (horizontalDirection < 0f)
+            {
+                spriteRenderer.flipX = true;
+            } else
+            {
+                spriteRenderer.flipX = false;
+            }
+        } else
+        {
+            playerAnimator.SetBool("Running", false);
         }
 
 
